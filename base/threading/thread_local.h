71441264c78f887959450fb501004640da5dde10
Hongbo Min <hongbo.min@intel.com>
2014-01-15 16:39:35 +0800

    [Android] Use the Chromium TLS for thread local storage allocation
    On Android ICS, the max number of thread local storage is limited to 64.
    Chromium TLS provides a mechanism to support large number of TLS slots
    which is independent of OS restictions.
    
    This commit is to make sure the ThreadLocalPointer uses the Chromium TLS
    mechnism instead of directly calling pthread_create_key, so that it can
    avoid the crash if the number of TLS slots allocated exceeds the max number
    allowed by OS.
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-841

--- base/threading/thread_local.h
+++ base/threading/thread_local.h
@@ -55,6 +55,7 @@
 
 namespace base {
 
+#if !defined(OS_ANDROID)
 template <typename Type>
 class ThreadLocalPointer {
  public:
@@ -74,6 +75,28 @@ class ThreadLocalPointer {
 
   DISALLOW_COPY_AND_ASSIGN(ThreadLocalPointer<Type>);
 };
+#else
+template <typename Type>
+ class ThreadLocalPointer {
+  public:
+   ThreadLocalPointer() {}
+
+   ~ThreadLocalPointer() { slot_.Free(); }
+
+   Type* Get() {
+     return static_cast<Type*>(slot_.Get());
+   }
+
+   void Set(Type* ptr) {
+     slot_.Set(const_cast<void*>(static_cast<const void*>(ptr)));
+   }
+
+  private:
+   ThreadLocalStorage::Slot slot_;
+
+   DISALLOW_COPY_AND_ASSIGN(ThreadLocalPointer<Type>);
+};
+#endif  // !OS_ANDROID
 
 class ThreadLocalBoolean {
  public:
